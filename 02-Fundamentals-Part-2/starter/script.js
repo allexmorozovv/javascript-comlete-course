57a2e24dfa64cfd2977d577947b8dae8f43222ac
allexmorozovv <allexmorozovv@gmail.com>
2022-10-18 16:31:46 +0300

    add fruitProcessor function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,14 @@
 'use strict'
+
+function fruitProcessor(apple, orange) {
+    console.log(apple, orange)
+    const juice = `Juice with ${apple} apples and ${orange} oranges `
+    return juice
+
+}
+
+const appleJuice = fruitProcessor(5, 0)
+console.log(appleJuice)
+const appleOrangeJuice =fruitProcessor(4,2)
+console.log(appleOrangeJuice)
+
