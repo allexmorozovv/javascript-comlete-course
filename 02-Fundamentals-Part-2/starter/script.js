fa7480e4ebdf9de248e0e22b7dae20b08f7a1d04
allexmorozovv <allexmorozovv@gmail.com>
2022-10-19 18:27:29 +0300

    add Arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -13,16 +13,31 @@
 // console.log(appleOrangeJuice)
 
 //Function declaration
-function ageCalc1(birthYear) {
-    return 2022 - birthYear
+// function ageCalc1(birthYear) {
+//     return 2022 - birthYear
+// }
+//
+// const age1 = ageCalc1(1977)
+//
+// //Function expression
+// const calcAge2 = function (birthYear) {
+//     return 2022 - birthYear
+// }
+// const age2 = calcAge2(1977)
+//
+// console.log(age1, age2)
+
+// //Arrow function
+// const calcAge3 = birthDay => 2022-birthDay
+// const age3=calcAge3(1977)
+//
+// console.log(age3)
+// console.log(calcAge3(1977))
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2022 - birthYear
+    const retirement = 65 - age
+    return ` ${firstName} retires in ${retirement}`
 }
-
-const age1 = ageCalc1(1977)
-
-//Function expression
-const calcAge2 = function (birthYear) {
-    return 2022 - birthYear
-}
-const age2 = calcAge2(1977)
-
-console.log(age1, age2)
+console.log(yearsUntilRetirement(1977,'Alex'))
+console.log(yearsUntilRetirement(1980,'Bob'))
