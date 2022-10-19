b9b4c85954b30c43d5bbf9565f07d418a4a7c82a
allexmorozovv <allexmorozovv@gmail.com>
2022-10-19 17:33:07 +0300

    add Function declaration and Function expression

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,14 +1,28 @@
 'use strict'
 
-function fruitProcessor(apple, orange) {
-    console.log(apple, orange)
-    const juice = `Juice with ${apple} apples and ${orange} oranges `
-    return juice
+// function fruitProcessor(apple, orange) {
+//     console.log(apple, orange)
+//     const juice = `Juice with ${apple} apples and ${orange} oranges `
+//     return juice
+//
+// }
+//
+// const appleJuice = fruitProcessor(5, 0)
+// console.log(appleJuice)
+// const appleOrangeJuice =fruitProcessor(4,2)
+// console.log(appleOrangeJuice)
 
+//Function declaration
+function ageCalc1(birthYear) {
+    return 2022 - birthYear
 }
 
-const appleJuice = fruitProcessor(5, 0)
-console.log(appleJuice)
-const appleOrangeJuice =fruitProcessor(4,2)
-console.log(appleOrangeJuice)
+const age1 = ageCalc1(1977)
 
+//Function expression
+const calcAge2 = function (birthYear) {
+    return 2022 - birthYear
+}
+const age2 = calcAge2(1977)
+
+console.log(age1, age2)
