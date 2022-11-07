b73862fab9e086de8a57910f743fa3b651a01ba0
allexmorozovv <allexmorozovv@gmail.com>
2022-11-07 18:10:00 +0300

    add Array alex

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -77,19 +77,34 @@
 // console.log(yearsUntilRetirement(1977, 'Alex'))
 // console.log(yearsUntilRetirement(1940, 'Bob'))
 
-const calcAverage = (a,b,c) => (a+b+c)/3
-const scoreDolphins=calcAverage(44,23,71)
-const scoreKoalas=calcAverage(65,54,49)
-console.log(scoreDolphins,scoreKoalas)
+// const calcAverage = (a, b, c) => (a + b + c) / 3
+// const scoreDolphins = calcAverage(44, 23, 71)
+// const scoreKoalas = calcAverage(65, 54, 49)
+// console.log(scoreDolphins, scoreKoalas)
+//
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//     if (avgDolphins >= avgKoalas * 2) {
+//         console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`)
+//     } else if (avgKoalas >= avgDolphins * 2) {
+//         console.log(`Koalas win (${avgKoalas}vs ${avgDolphins}`)
+//     } else {
+//         console.log(`No team wins...`)
+//     }
+// }
+// checkWinner(scoreDolphins, scoreKoalas)
 
-const checkWinner=function (avgDolphins,avgKoalas){
-    if (avgDolphins>=avgKoalas*2){
-        console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`)
-    }else if (avgKoalas>=avgDolphins*2){
-        console.log(`Koalas win (${avgKoalas}vs ${avgDolphins}`)
-    }else {
-        console.log(`No team wins...`)
-    }
-}
-checkWinner(scoreDolphins,scoreKoalas)
+const friend1 = 'Alex'
+const friend2 = 'Bob'
+const friend3 = 'Tom'
+
+const friends = ['Alex', 'Bob', 'Tom']
+console.log(friends)
+
+const years = new Array(1991, 1992, 2001, 2000)
+
+friends[2] = 'John'
+
+const firstName = 'Alex'
+const alex = [firstName, 'Frost', 2022 - 1977, friends]
+console.log(alex)
 
